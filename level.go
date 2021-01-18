57c255bd0c51f25b6fc9e37c1d18eb90b26b49c4
Richard Ashkettle <richard.ashkettle@gmail.com>
2021-01-18 14:33:08 -0500

    map generation working

--- level.go
+++ level.go
@@ -65,12 +65,13 @@ func (level *Level) GenerateLevelTiles() {
 	gd := NewGameData()
 	tiles := level.createTiles()
 	level.Tiles = tiles
+	contains_rooms := false
 
 	for idx := 0; idx < MAX_ROOMS; idx++ {
 		w := GetRandomBetween(MIN_SIZE, MAX_SIZE)
 		h := GetRandomBetween(MIN_SIZE, MAX_SIZE)
-		x := GetDiceRoll(gd.ScreenWidth-w-1) - 1
-		y := GetDiceRoll(gd.ScreenHeight-h-1) - 1
+		x := GetDiceRoll(gd.ScreenWidth - w - 1)
+		y := GetDiceRoll(gd.ScreenHeight - h - 1)
 		new_room := NewRect(x, y, w, h)
 
 		okToAdd := true
@@ -83,15 +84,58 @@ func (level *Level) GenerateLevelTiles() {
 		}
 		if okToAdd {
 			level.createRoom(new_room)
-			if len(level.Rooms) == 0 {
+			if contains_rooms {
+				newX, newY := new_room.Center()
+				prevX, prevY := level.Rooms[len(level.Rooms)-1].Center()
+				coinflip := GetDiceRoll(2)
+				if coinflip == 2 {
+					level.createHorizontalTunnel(prevX, newX, prevY)
+					level.createVerticalTunnel(prevY, newY, newX)
+
+				} else {
+					level.createHorizontalTunnel(prevX, newX, newY)
+					level.createVerticalTunnel(prevY, newY, prevX)
+				}
 
 			}
 
 			level.Rooms = append(level.Rooms, new_room)
-
+			contains_rooms = true
 		}
 	}
 
+}
+
+func (level *Level) createHorizontalTunnel(x1 int, x2 int, y int) {
+	gd := NewGameData()
+	for x := min(x1, x2); x < max(x1, x2)+1; x++ {
+		index := level.GetIndexFromXY(x, y)
+		if index > 0 && index < gd.ScreenWidth*gd.ScreenHeight {
+			level.Tiles[index].Blocked = false
+			floor, _, err := ebitenutil.NewImageFromFile("assets/floor.png")
+			if err != nil {
+				log.Fatal(err)
+			}
+			level.Tiles[index].Image = floor
+
+		}
+	}
+}
+
+func (level *Level) createVerticalTunnel(y1 int, y2 int, x int) {
+	gd := NewGameData()
+	for y := min(y1, y2); y < max(y1, y2)+1; y++ {
+		index := level.GetIndexFromXY(x, y)
+		if index > 0 && index < gd.ScreenWidth*gd.ScreenHeight {
+			level.Tiles[index].Blocked = false
+			floor, _, err := ebitenutil.NewImageFromFile("assets/floor.png")
+			if err != nil {
+				log.Fatal(err)
+			}
+			level.Tiles[index].Image = floor
+
+		}
+	}
 }
 
 //createTiles creates a map of all walls as a baseline for carving out a level.
@@ -131,4 +175,20 @@ func (level *Level) createRoom(room Rect) {
 			level.Tiles[index].Image = floor
 		}
 	}
+}
+
+// Max returns the larger of x or y.
+func max(x, y int) int {
+	if x < y {
+		return y
+	}
+	return x
+}
+
+// Min returns the smaller of x or y.
+func min(x, y int) int {
+	if x > y {
+		return y
+	}
+	return x
 }
